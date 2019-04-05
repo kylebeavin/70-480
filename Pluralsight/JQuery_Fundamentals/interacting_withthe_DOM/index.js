779a957aecf98d7765b46545f5da5ff919c2b95e
kyle beavin <kylebeavin@gmail.com>
2019-04-05 16:06:15 -0400

    stamping out html to play with

--- Pluralsight/JQuery_Fundamentals/interacting_withthe_DOM/index.js
+++ Pluralsight/JQuery_Fundamentals/interacting_withthe_DOM/index.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
-    
+    $('table,thead,tbody,th,tr,td').css('border', '1px solid black').css('padding', '10px');
+    // .each(function(index,Element)) is used to iterate through jQuery objects:
+    $('td').each(function (index) {
+    });
 });
