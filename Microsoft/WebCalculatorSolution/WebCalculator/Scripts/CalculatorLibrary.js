3667356bb67ad07bf69adb83fe93f608a57f2c4b
kyle beavin <kylebeavin@gmail.com>
2019-04-15 16:33:13 -0400

    web calculator with test suite

---
+++ Microsoft/WebCalculatorSolution/WebCalculator/Scripts/CalculatorLibrary.js
@@ -0,0 +1,59 @@
+﻿// initialize variable that can be called each time a test is run
+var txtInput;
+var txtResult;
+
+function initialize() {
+    // execute 10 times
+    for (var i = 0; i < 10; i++) {
+        // subscribe to 'click' event and call numberClick
+        document.getElementById('btn'+i).addEventListener('click', numberClick, false);
+    }
+
+    // locate the Input text boxes
+    txtInput = document.getElementById('txtInput');
+    txtResult = document.getElementById('txtResult');
+
+    //subscribe to 'click' event and call functions
+    document.getElementById('btnPlus').addEventListener('click', plusClick, false);
+    document.getElementById('btnMinus').addEventListener('click', minusClick, false);
+    document.getElementById('btnClearEntry').addEventListener('click', clearEntry, false);
+    document.getElementById('btnClear').addEventListener('click', clear, false);
+    document.getElementById('btnMultiply').addEventListener('click', multiplyClick, false);
+    document.getElementById('btnDivide').addEventListener('click', divideClick, false);
+    clear();
+}
+
+function numberClick() {
+    // read innerText property of a button that was clicked and append it to txtInput if 0 replace with new value
+    txtInput.value = txtInput.value == '0' ?
+        this.innerText : txtInput.value + this.innerText;
+}
+
+function plusClick() {
+    txtResult.value = Number(txtResult.value) + Number(txtInput.value);
+    clearEntry();
+}
+
+function minusClick() {
+    txtResult.value = Number(txtResult.value) - Number(txtInput.value);
+    clearEntry();
+}
+
+function clearEntry() {
+    txtInput.value = '0';
+}
+
+function clear() {
+    txtInput.value = '0';
+    txtResult.value = '0';
+}
+
+function multiplyClick() {
+    txtResult.value = Number(txtResult.value) * Number(txtInput.value);
+    clearEntry();
+}
+
+function divideClick() {
+    txtResult.value = Number(txtResult.value) / Number(txtInput.value);
+    clearEntry();
+}
---
+++ Microsoft/WebCalculatorSolution/WebCalculator/Scripts/tests.js
@@ -0,0 +1,102 @@
+﻿// the first param is the name of the module
+// second param defines an object that has a setup property
+// the setup property is assigned a function expression that calls the initialize function before each test
+module('Calculator Test Suite', { setup: function () { initialize(); } });
+
+test("Initialize Test", function () {
+    expect(2);
+    txtInput.value = '0';
+    txtResult.value = '0';
+    var expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+});
+
+test("Button Click Test", function () {
+    //sets button Quantity to 10
+    var buttonQuantity = 10;
+
+    // buttonQuantity calls the expect function to set the quantity of assertions
+    expect(buttonQuantity * 2);
+
+    // for loop executes 10 times to obtain a referenct to the appropriate button
+    for (var i = 0; i < buttonQuantity; i++) {
+
+        // trigger the click event
+        var btn = document.getElementById('btn' + i);
+        QUnit.triggerEvent(btn, "click");
+
+        // retrieve the result
+        var result = txtInput.value[txtInput.value.length - 1];
+        var expected = String(i);
+
+        // perform an assertion that checks the last character of txtInput
+        equal(result, expected, 'Expected value: ' + expected + ' Actual value: ' + result);
+
+        // perform an assertion that checks the length of txtInput
+        var expectedLength = i < 2 ? 1 : i;
+        equal(txtInput.value.length, expectedLength, 'Expected string length: ' + expectedLength + ' Actual value: ' + txtInput.value.length);
+    }
+});
+
+test("Add Test", function () {
+    expect(2);
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnPlus, "click");
+    var expected = '30';
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+test("Subtract Test", function () {
+    expect(2);
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnMinus, "click");
+    var expected = '10';
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+test("Clear Entry Test", function () {
+    expect(1);
+    txtInput.value = '10';
+    QUnit.triggerEvent(btnClearEntry, "click");
+    var expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+test("Clear Test", function () {
+    expect(2);
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnClear, "click");
+    var expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+});
+
+test("Multiply Test", function () {
+    expect(2);
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnMultiply, "click");
+    var expected = '200';
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
+
+test("Divide Test", function () {
+    expect(2);
+    txtInput.value = '10';
+    txtResult.value = '20';
+    QUnit.triggerEvent(btnDivide, "click");
+    var expected = '2';
+    equal(txtResult.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtResult.value);
+    expected = '0';
+    equal(txtInput.value, expected, 'Expected value: ' + expected + ' Actual value: ' + txtInput.value);
+});
