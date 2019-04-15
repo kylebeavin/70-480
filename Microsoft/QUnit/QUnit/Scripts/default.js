2f14620fd51af82eabf520d4c1db2b4797598634
kyle beavin <kylebeavin@gmail.com>
2019-04-15 11:56:37 -0400

    added new solution QUnit tests

---
+++ Microsoft/QUnit/QUnit/Scripts/default.js
@@ -0,0 +1,10 @@
+﻿
+var greeting = "Hello World";
+
+function areaOfPizzaSlice(diameter, slicesPerPizza) {
+    return areaOfPizza(diameter) / slicesPerPizza;
+    function areaOfPizza(diameter) {
+        var radius = diameter / 2;
+        return 3.141592 * radius * radius;
+    }
+}
---
+++ Microsoft/QUnit/QUnit/Scripts/tests.js
@@ -0,0 +1,8 @@
+﻿
+test("A Hello World Test", 1, function () {
+    equal(greeting, "Hello World", "Expect greeting of Hello World");
+});
+
+test("Area of Pizza Slice", 1, function () {
+    equal(areaOfPizzaSlice(18, 8), 31.808619, "Expected 31.808619");
+});
