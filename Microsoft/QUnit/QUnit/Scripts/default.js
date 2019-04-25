7954a5c851f017f20d692e534c02c395ad7396f1
kyle beavin <kylebeavin@gmail.com>
2019-04-25 16:47:54 -0400

    unit testing javascript objects

--- Microsoft/QUnit/QUnit/Scripts/default.js
+++ Microsoft/QUnit/QUnit/Scripts/default.js
@@ -8,3 +8,34 @@ function areaOfPizzaSlice(diameter, slicesPerPizza) {
         return 3.141592 * radius * radius;
     }
 }
+// Object Literals
+var car1 = {
+    year: 2000,
+    make: 'Ford',
+    model: 'Fusion',
+    repairs: ['repair1', 'repair2', 'repair3'], //array property
+    getInfo: function () {
+        return 'Vehicle: ' + this.year + ' ' + this.make + ' ' + this.model;
+    }
+};
+
+var car2 = {
+    year: 2010,
+    make: 'BMW',
+    model: 'Z4',
+    getInfo: function () {
+        return 'Vehicle: ' + this.year + ' ' + this.make + ' ' + this.model;
+    }
+};
+
+// Creating dynamic objects by using the factory pattern
+function getVehicle(theYear, theMake, theModel) {
+    var vehicle = new Object();
+    vehicle.year = theYear,
+    vehicle.make = theMake,
+    vehicle.model = theModel,
+    vehicle.getInfo = function () {
+        return 'Vehicle: ' + this.year + ' ' + this.make + ' ' + this.model;
+        };
+    return vehicle; // this is of type Object not type vehicle
+}
--- Microsoft/QUnit/QUnit/Scripts/tests.js
+++ Microsoft/QUnit/QUnit/Scripts/tests.js
@@ -6,3 +6,27 @@ test("A Hello World Test", 1, function () {
 test("Area of Pizza Slice", 1, function () {
     equal(areaOfPizzaSlice(18, 8), 31.808619, "Expected 31.808619");
 });
+
+// checks to make sure each object contains the data thats expected
+test("Object Literal Test", function () {
+    expect(2);
+    var expected = 'Vehicle: 2000 Ford Fusion';
+    var actual = car1.getInfo();
+    equal(actual, expected, 'Expected value: ' + expected + ' Actual value: ' + actual);
+    var expected = 'Vehicle: 2010 BMW Z4';
+    var actual = car2.getInfo();
+    equal(actual, expected, 'Expected value: ' + expected + ' Actual value: ' + actual);
+});
+
+// demonstrates the creation of multiple instances
+test("Create Instances Test Using Factory Pattern", function () {
+    expect(2);
+    var car1 = getVehicle(2000, 'Ford', 'Fusion');
+    var car2 = getVehicle(2010, 'BMW', 'Z4');
+    var expected = 'Vehicle: 2000 Ford Fusion';
+    var actual = car1.getInfo();
+    equal(actual, expected, 'Expected value: ' + ' Actual value: ' + actual);
+    var expected = 'Vehicle: 2010 BMW Z4';
+    var actual = car2.getInfo();
+    equal(actual, expected, 'Expected value: ' + ' Actual value: ' + actual);
+});
