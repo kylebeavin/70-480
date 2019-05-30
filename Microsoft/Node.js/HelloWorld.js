ec5e6dc297ceeaf802cfe3ff31caa9ea7bf98a38
kyle beavin <kylebeavin@gmail.com>
2019-05-30 14:50:13 -0400

    Chapter 8 Websites and services

---
+++ Microsoft/Node.js/HelloWorld.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function (request, response){
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World from Node.js!\n');
+    console.log('Handled Request');
+}).listen(8080, 'localhost');
+
+console.log('Server running at http://localhost:8080/');
