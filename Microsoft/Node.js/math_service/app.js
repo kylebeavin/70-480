be89ecd73daac40e1f86d02bf5065bc213731361
kyle beavin <kylebeavin@gmail.com>
2019-05-31 11:17:10 -0400

    creating a RESTful web service by using Node.js

---
+++ Microsoft/Node.js/math_service/app.js
@@ -0,0 +1,20 @@
+﻿var express = require('express');
+var app = express();
+var formidable = require('formidable');
+var math = require('math_example');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/addition', function (request, response) {
+    var x = Number(request.query.x),
+        y = Number(request.query.y),
+        result = math.addition(x, y);
+
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end('{ "result": ' + result + '}');
+    console.log('Handled addition request for x=' + x + ' : y=' + y);
+});
+
+var port = 8080;
+app.listen(port);
+console.log('Listening on port: ' + port);
