c3f721c2b7d091b5ada781e422fac1040531fb54
kyle beavin <kylebeavin@gmail.com>
2019-05-31 12:51:17 -0400

    using ajax to call a web service

--- Microsoft/Node.js/math_service/app.js
+++ Microsoft/Node.js/math_service/app.js
@@ -15,6 +15,42 @@ app.get('/addition', function (request, response) {
     console.log('Handled addition request for x=' + x + ' : y=' + y);
 });
 
+app.post('/subtraction', function (request, response) {
+    var form = new formidable.IncomingForm();
+    form.parse(request, function (err, fields) {
+        var x = Number(fields.x),
+            y = Number(fields.y),
+            result = math.subtraction(x, y);
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end('{ "result": ' + result + '}');
+        console.log('Handled subtraction request for x=' + x + ' : y=' + y);
+    });
+});
+
+app.put('/multiply', function (request, response) {
+    var form = new formidable.IncomingForm();
+    form.parse(request, function (err, fields) {
+        var x = Number(fields.x),
+            y = Number(fields.y),
+            result = math.multiplication(x, y);
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end('{ "result": ' + result + '}');
+        console.log('Handled multiplication request for x=' + x + ' : y=' + y);
+    });
+});
+
+app.delete('/divide', function (request, response) {
+    var form = new formidable.IncomingForm();
+    form.parse(request, function (err, fields) {
+        var x = Number(fields.x),
+            y = Number(fields.y),
+            result = math.division(x, y);
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end('{ "result": ' + result + '}');
+        console.log('Handled division request for x=' + x + ' : y=' + y);
+    });
+});
+
 var port = 8080;
 app.listen(port);
 console.log('Listening on port: ' + port);
---
+++ Microsoft/Node.js/math_service/public/Scripts/default.js
@@ -0,0 +1,56 @@
+﻿$(document).ready(function () {
+    $('#btnAdd').on('click', addNumbers);
+    $('#btnSubtract').on('click', subtractNumbers);
+    $('#btnMultiplication').on('click', multiplyNumbers);
+    $('#btnDivision').on('click', divideNumbers);
+});
+
+function addNumbers() {
+    var x = $('#x').val();
+    var y = $('#y').val();
+    var data = { "x": x, "y": y };
+    $.getJSON('/addition', data, function (data) {
+        $('#result').html(data.result);
+    });
+}
+
+function subtractNumbers() {
+    var x = $('#x').val();
+    var y = $('#y').val();
+    var data = { "x": x, "y": y };
+    $.post('/subtraction', data, function (data) {
+        $('#result').html(data.result);
+    }, 'json');
+}
+
+function multiplyNumbers() {
+    var x = $('#x').val();
+    var y = $('#y').val();
+    var data = { "x": x, "y": y };
+    $.ajax({
+        url: '/multiply',
+        data: data,
+        type: 'PUT',
+        dataType: 'json',
+        cache: false,
+        success: function (data) {
+            $('#result').html(data.result);
+        }
+    });
+}
+
+function divideNumbers() {
+    var x = $('#x').val();
+    var y = $('#y').val();
+    var data = { "x": x, "y": y };
+    $.ajax({
+        url: '/divide',
+        data: data,
+        type: 'DELETE',
+        dataType: 'json',
+        cache: false,
+        success: function (data) {
+            $('#result').html(data.result);
+        }
+    });
+}
