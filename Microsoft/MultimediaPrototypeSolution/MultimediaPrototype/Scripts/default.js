054cd0fe762752c7a6940f47b059a8960b40cf39
kyle beavin <kylebeavin@gmail.com>
2019-06-03 15:03:15 -0400

    HTML5 multimedia

---
+++ Microsoft/MultimediaPrototypeSolution/MultimediaPrototype/Scripts/default.js
@@ -0,0 +1,6 @@
+﻿/// <reference path="jquery-3.4.1.js" />
+$(document).ready(function () {
+    $('#media').on('play', function () {
+        $('#message').html($('#media')[0].currentSrc);
+    });
+});
