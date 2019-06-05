19dcd2ce791fdfbb835fceec4d4ea767d7af6f3f
kyle beavin <kylebeavin@gmail.com>
2019-06-05 12:46:32 -0400

    File Drag and Drop

---
+++ Microsoft/Drag_and_Drop/Drag_and_Drop/Scripts/FileDragAndDrop.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="jquery-2.2.4.js" />
+
+$(document).ready(function () {
+    jQuery.event.props.push('dataTransfer');
+    $('#target').on('dragenter', preventDefault);
+    $('#target').on('dragover', preventDefault);
+    $('#target').on('drop', dropItem);
+});
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function dropItem(e) {
+    var files = e.dataTransfer.files
+        , $table = $('#fileInfo')
+        , i = 0;
+    $table.html(
+        '<thead><tr><th>Name</th><th>Type</th><th>Size</th></tr></thead>');
+    for (i = 0; i < files.length; i++) {
+        $('<tr><td>'
+            + files[i].name + '</td><td>'
+            + files[i].type + '</td><td>'
+            + files[i].size + '</td></tr>').appendTo($table);
+    }
+    preventDefault(e);
+}
