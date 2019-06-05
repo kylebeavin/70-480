47b3f27e17bf3f2e35e46db24052712d2cc4c75c
kyle beavin <kylebeavin@gmail.com>
2019-06-05 15:14:05 -0400

    excercise1 Map your current positions

--- Microsoft/PositionMapper/PositionMapper/Scripts/GeoLocation.js
+++ Microsoft/PositionMapper/PositionMapper/Scripts/GeoLocation.js
@@ -1,7 +1,13 @@
 ﻿/// <reference path="jquery-3.4.1.js" />
 
+var watchId = 0;
+
 $(document).ready(function () {
-    getLocation();
+
+    // getLocation();
+
+    $('#startMonitoring').on('click', getLocation);
+    $('#stopMonitoring').on('click', endWatch);
 });
 
 function supportsGeolocation() {
@@ -16,13 +22,23 @@ function getLocation() {
     if (supportsGeolocation()) {
         var options = {
             enableHighAccuracy: true,
-            timeout: 3000,
-            maximumAge: 20000
+            // timeout: 3000,
+            // maximumAge: 20000
         }
-        navigator.geolocation.getCurrentPosition(showPosition, showError, options);
+        // navigator.geolocation.getCurrentPosition(showPosition, showError, options);
+
+        watchId = navigator.geolocation.watchPosition(showPosition, showError, options);
     }
     else {
         showMessage("Geolocation is not supported by this browser");
+    }
+}
+
+function endWatch() {
+    if (watchId != 0) {
+        navigator.geolocation.clearWatch(watchId);
+        watchId = 0;
+        showMessage("Monitoring ended.");
     }
 }
 
@@ -48,4 +64,21 @@ function showError(error) {
             showMessage("An unknown error occurred.");
             break;
     }
+}
+
+// haversine formula
+function getDistance(lat1, lon1, lat2, lon2) {
+    var earthRadius = 3959; //miles
+    var latRadians = getRadians(lat2 - lat1);
+    var lonRadians = getRadians(lon2 - lon1);
+    var a = Math.sin(latRadians / 2) * Math.sin(latRadians / 2) +
+        Math.cos(getRadians(lat1)) * Math.cos(getRadians(lat2)) *
+        Math.sin(lonRadians / 2) * Math.sin(lonRadians / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var distance = earthRadius * c;
+    return distance;
+}
+
+function getRadians(latlongDistance) {
+    return latlongDistance * Math.PI / 180;
 }
