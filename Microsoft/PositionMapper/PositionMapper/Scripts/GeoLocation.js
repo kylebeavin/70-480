5c906ee8972b4df1b6744d19c1afc9220c4f5f36
kyle beavin <kylebeavin@gmail.com>
2019-06-05 14:32:57 -0400

    Making your HTML location-aware

---
+++ Microsoft/PositionMapper/PositionMapper/Scripts/GeoLocation.js
@@ -0,0 +1,29 @@
+﻿/// <reference path="jquery-3.4.1.js" />
+
+$(document).ready(function () {
+    getLocation();
+});
+
+function supportsGeolocation() {
+    return 'geolocation' in navigator;
+}
+
+function showMessage(message) {
+    $('#message').html(message);
+}
+
+function getLocation() {
+    if (supportsGeolocation()) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        showMessage("Geolocation is not supported by this browser");
+    }
+}
+
+function showPosition(position) {
+    var datetime = new Date(position.timestamp).toLocaleString();
+    showMessage("Latitude: " + position.coords.latitude + "<br />"
+        + "Longitude: " + position.coords.longitude + "<br />"
+        + "Timestamp: " + datetime);
+}
