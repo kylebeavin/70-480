c80e7063e940e9a1fb863c84713f37994c5c2e32
kyle beavin <kylebeavin@gmail.com>
2019-06-05 13:52:27 -0400

    add scramble and winner

--- Microsoft/Drag_and_Drop/NumberScramble/NumberScramble/Scripts/default.js
+++ Microsoft/Drag_and_Drop/NumberScramble/NumberScramble/Scripts/default.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
     $('#gameBoard').on('dragenter', preventDefault);
     $('#gameBoard').on('dragover', preventDefault);
     $('#gameBoard').on('drop', drop);
+    scramble();
 });
 
 function createBoard() {
@@ -55,6 +56,7 @@ function drop(e) {
         if (emptySquare != destinationLocation) return;
         var sourceLocation = Number(e.dataTransfer.getData('text'));
         moveTile(sourceLocation);
+        checkForWinner();
     }
 }
 
@@ -72,4 +74,38 @@ function swapTileAndEmptySquare(sourceLocation) {
     var $target = $('#square' + emptySquare);
     $draggedItem.appendTo($target);
     emptySquare = sourceLocation;
+}
+
+function scramble() {
+    for (var i = 0; i < 128; i++) {
+        var random = Math.random()
+        var sourceLocation;
+        if (random < 0.5) {
+            var column = emptySquare % 4
+            if (column == 0 || (random < 0.25 && column != 3)) {
+                sourceLocation = emptySquare + 1;
+            }
+            else {
+                sourceLocation = emptySquare - 1;
+            }
+        }
+        else {
+            var row = Math.floor(emptySquare / 4)
+            if (row == 0 || (random < 0.75 && row != 3)) {
+                sourceLocation = emptySquare + 4;
+            }
+            else {
+                sourceLocation = emptySquare - 4;
+            }
+        }
+        swapTileAndEmptySquare(sourceLocation);
+    }
+}
+
+function checkForWinner() {
+    if (emptySquare != squareCount - 1) return;
+    for (var i = 0; i < emptySquare; i++) {
+        if ($('#tile' + i).parent().attr('id') != 'square' + i) return;
+    }
+    $('#message').html('Winner!');
 }
