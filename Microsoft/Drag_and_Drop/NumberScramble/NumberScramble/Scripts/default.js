284bb1107e57b35d5b5ada83e227ae77fbc91e0a
kyle beavin <kylebeavin@gmail.com>
2019-06-05 13:36:09 -0400

    added drag and drop

--- Microsoft/Drag_and_Drop/NumberScramble/NumberScramble/Scripts/default.js
+++ Microsoft/Drag_and_Drop/NumberScramble/NumberScramble/Scripts/default.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
     jQuery.event.props.push('dataTransfer');
     createBoard();
     addTiles();
+    $('#gameBoard').on('dragstart', dragStarted);
+    $('#gameBoard').on('dragend', dragEnded);
+    $('#gameBoard').on('dragenter', preventDefault);
+    $('#gameBoard').on('dragover', preventDefault);
+    $('#gameBoard').on('drop', drop);
 });
 
 function createBoard() {
@@ -25,4 +30,46 @@ function addTiles() {
             + '" class="tile">' + (i + 1) + '</div>');
         $tile.appendTo($square);
     }
+}
+
+function dragStarted(e) {
+    var $tile = $(e.target)
+    $tile.addClass('dragged');
+    var sourceLocation = $tile.parent().data('square');
+    e.dataTransfer.setData('text', sourceLocation.toString());
+    e.dataTransfer.effectAllowed = 'move';
+}
+
+function dragEnded(e) {
+    $(e.target).removeClass('dragged');
+}
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function drop(e) {
+    var $square = $(e.target);
+    if ($square.hasClass('square')) {
+        var destinationLocation = $square.data('square');
+        if (emptySquare != destinationLocation) return;
+        var sourceLocation = Number(e.dataTransfer.getData('text'));
+        moveTile(sourceLocation);
+    }
+}
+
+function moveTile(sourceLocation) {
+    var distance = sourceLocation - emptySquare;
+    if (distance < 0) distance = -(distance);
+    if (distance == 1 || distance == 4) {
+        swapTileAndEmptySquare(sourceLocation);
+    }
+}
+
+function swapTileAndEmptySquare(sourceLocation) {
+    var $draggedItem = $('#square' + sourceLocation).children();
+    $draggedItem.detach();
+    var $target = $('#square' + emptySquare);
+    $draggedItem.appendTo($target);
+    emptySquare = sourceLocation;
 }
