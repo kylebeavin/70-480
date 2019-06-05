7f85fc18a44866435269636253eede0606facd80
kyle beavin <kylebeavin@gmail.com>
2019-06-05 12:01:09 -0400

    jquery drag n drop 'dataTransfer'

---
+++ Microsoft/Drag_and_Drop/Drag_and_Drop/Scripts/CarList.js
@@ -0,0 +1,28 @@
+﻿/// <reference path="jquery-2.2.4.js" />
+
+$(document).ready(function () {
+    jQuery.event.props.push('dataTransfer');
+    $('#carList').on('dragstart', dragging);
+    $('#favoriteCars').on('dragenter', preventDefault);
+    $('#favoriteCars').on('dragover', preventDefault);
+    $('#favoriteCars').on('drop', dropItem);
+});
+
+function dragging(e) {
+    var val = e.target.getAttribute('data-value');
+    e.dataTransfer.setData('text', val);
+    e.dataTransfer.effectAllowed = 'copy';
+}
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+function dropItem(e) {
+    var data = e.dataTransfer.getData('text').split(',');
+    if (data[0] == 'car') {
+        var li = document.createElement('li');
+        li.textContent = data[1];
+        e.target.appendChild(li);
+    }
+}
