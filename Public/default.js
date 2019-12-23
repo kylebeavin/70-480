d5393d3f55b048d160597b597ef95909bc8821ed
kyle beavin <kylebeavin@gmail.com>
2019-12-22 22:14:51 -0500

    commit

---
+++ Public/default.js
@@ -0,0 +1,14 @@
+function GetCommission(){
+    console.log("10")
+}
+
+
+function Customer() {
+    this.name = name
+}
+
+Customer.prototype.GetCommission = function(){
+    console.log("$5");
+}
+
+console.log(Customer);
